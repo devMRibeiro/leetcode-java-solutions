32b10c9fdd82d848ecf8ccdfd3b9d66a796ed981
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-01-09 23:22:35 -0300

    Solved

--- src/com/solutions/CountPrefixSuffixPairs.java
+++ src/com/solutions/CountPrefixSuffixPairs.java
@@ -20,12 +20,12 @@ package com.solutions;
  * 
  */
 public class CountPrefixSuffixPairs {
-	
+
 	public static int countPrefixSuffixPairs(String[] words) {
 
 		int wLen = words.length;
 		int qt = 0;
-		
+
 		for (int i = 0; i < wLen; i++) {
 			for (int j = i + 1; j < wLen; j++) {
 				if (isPrefixAndSuffix(words[i], words[j]))
@@ -34,7 +34,7 @@ public class CountPrefixSuffixPairs {
 		}
 		return qt;
   }
-	
+
 	private static boolean isPrefixAndSuffix(String str1, String str2) {
 		return str1.length() <= str2.length() && str2.startsWith(str1) && str2.endsWith(str1);
 	}
---
+++ src/com/solutions/CountingWordsWithAPrefix.java
@@ -0,0 +1,32 @@
+package com.solutions;
+
+
+/**
+ * You are given an array of strings words and a string pref.
+ * Return the number of strings in words that contain pref as a prefix.
+ *  
+ * Example 1:
+ * Input: words = ["pay","attention","practice","attend"], pref = "at"
+ * Output: 2
+ * Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+ * 
+ * Example 2:
+ * Input: words = ["leetcode","win","loops","success"], pref = "code"
+ * Output: 0
+ * Explanation: There are no strings that contain "code" as a prefix.
+ *
+ * @return the number of strings in words that contain pref as a prefix.
+ */
+public class CountingWordsWithAPrefix {
+
+	public int prefixCount(String[] words, String pref) {
+
+		int ans = 0;
+
+		for (int i = 0; i < words.length; i++)
+			if (words[i].startsWith(pref))
+				ans += 1;
+
+		return ans;
+  }
+}
