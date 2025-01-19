10c6138e42f1e2c8d47b604c377a644adff37abb
MaikRibeiro <michael.douglas2020@hotmail.com>
2025-01-18 23:46:08 -0300

    MissingNumber: Solved

---
+++ src/com/solutions/MissingNumber.java
@@ -0,0 +1,17 @@
+package com.solutions;
+
+public class MissingNumber {
+	public static int missingNumber(int[] nums) {
+		
+		int n = nums.length;
+    int calc = (n * (n + 1)) / 2;
+
+    for (int num : nums)
+        calc -= num;
+    
+    if (calc != 0)
+        return calc;
+
+    return 0;
+	}
+}
